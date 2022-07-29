c1ee55e4ddd7d3aff6237618998399e4ce597109
Farouq <sonarfarouq@gmail.com>
2022-07-30 01:19:03 +0300

    update with a shop page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import './App.css';
 import {Routes ,  Route} from 'react-router-dom';
 
-import  HomePage  from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+import ThemeSwitcher from "./ThemeSwitcher";
 
-const HatsPage= () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 function App() {
   return (
     <div>
       <Routes>
       <Route exact path = "/" element= {<HomePage/>}></Route>
-      <Route exact path = "/hats" element ={<HatsPage/>}></Route>
+      <Route exact path = "/shop" element ={<ShopPage/>}></Route>
       </Routes>
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// // Bootstrap CSS
+// import "bootstrap/dist/css/bootstrap.min.css";
+// // Bootstrap Bundle JS
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,3 +20,4 @@ root.render(
   </React.StrictMode>
 );
 
+export default App;
