7f2bcb2ba6697b4c0af82096c881de5e013f7165
Farouq <sonarfarouq@gmail.com>
2022-10-19 01:31:53 +0300

    new padding and design for the buttons 19/10

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,7 +13,8 @@ const Header = ({currentUser})=>(
         <div className="options">
             <Link className="option" to='/shop'>SHOP</Link>
             <Link className="option" to='/shop'>CONTACT</Link>
-            {currentUser ? (<div className="option" onClick={()=>auth.signOut()}> SIGN OUT
+            {
+            currentUser ? (<div className="option" onClick={()=>auth.signOut()}> SIGN OUT
             </div> )
             :( <Link className="option" to='/signIn'>SIGN IN</Link>)
             }
--- src/sign-in/sign-in.component.jsx
+++ src/sign-in/sign-in.component.jsx
@@ -32,13 +32,13 @@ class SignIn extends React.Component{
                     <FormInput name="email"  type ="email" value={this.state.email}  handleChange = {this.handelChange}  label = "email" required/>
                     
                     <FormInput name="password"  type ="password" value={this.state.password} handleChange= {this.handelChange} label = "password" required/>
-
+                    <div className='buttons'>
                     <CustomButton type = "submit"> Sign in </CustomButton>
-                    
                     <CustomButton onClick ={signInWithGoogle} isGoogleSignIn>
                         {''}
                     Sign in with Google {''}
                     </CustomButton>
+                    </div>
                 </form>
             </div>
         )
